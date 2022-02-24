32d8bdfc5fb58e0cba7416094560e19a9f588297
dbdudwo <100177702+dbdudwo@users.noreply.github.com>
2022-02-24 15:08:11 +0900

    Update models.py

--- users/models.py
+++ users/models.py
@@ -7,8 +7,4 @@ class Member(AbstractUser):
                               blank=True,
                               null=True)
     nickname = models.CharField(max_length=20,
-<<<<<<< HEAD
                                 unique=True)
-=======
-                                 unique=True)
->>>>>>> fcb7fb8 (commit)

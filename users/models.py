1c56f71f2395b245e776bd054b940df0a4d07882
Jaesung Cho <shcho950@naver.com>
2022-02-24 15:46:10 +0900

    test

--- users/models.py
+++ users/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+
+
 class Member(AbstractUser):
     mobile = models.CharField(max_length=20)
     image = models.ImageField(upload_to=settings.MEDIA_ROOT,
                               blank=True,
                               null=True)
     nickname = models.CharField(max_length=20,
-<<<<<<< HEAD
                                 unique=True)
-=======
-                                 unique=True)
->>>>>>> fcb7fb8 (commit)

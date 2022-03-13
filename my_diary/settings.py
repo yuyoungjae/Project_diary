2a1574005859fe0d0bd60dcb309c4725947268f8
yuyoungjae <yuyj97@naver.com>
2022-03-13 18:31:30 +0900

    진진진진짜최종

--- my_diary/settings.py
+++ my_diary/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'my_diary_db',
         'USER': 'root',
-        'PASSWORD': 'whwotjd123',
+        'PASSWORD': 'yu974346!',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
